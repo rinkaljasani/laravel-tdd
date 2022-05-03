3ebc92b738d3c9e792e3fdbdea67500b839df913
rinkal_jasani <rinkal.j@yudiz.com>
2022-05-03 15:05:35 +0530

    create create list, model,add field in model, migration,

--- tests/Feature/TodoListTest.php
+++ tests/Feature/TodoListTest.php
@@ -2,20 +2,17 @@
 
 namespace Tests\Feature;
 
+use App\Models\TodoList;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 class TodoListTest extends TestCase
 {
-    /**
-     * A basic feature test example.
-     *
-     * @return void
-     */
+    use RefreshDatabase;
     public function test_todo_list_fetch()
     {
-        
+        TodoList::create(['name' => 'test']);
         $response = $this->getJson(route('todo-list.index'));
         $this->assertEquals(1,count($response->json()));
     }
